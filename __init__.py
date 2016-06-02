ba9c5a3b795c439b7b2f33ffb11f9cd537a7f3bb
Nimna <nimna@nimna.me>
2016-06-02 13:32:13 +0000

    Added and mapped the sign up method to the sign up page.

--- __init__.py
+++ __init__.py
@@ -7,13 +7,14 @@ import os
 import sys
 from flask import *
 from functools import wraps
-from flask.ext.mysql import MySQL
+from flaskext.mysql import MySQL
+# from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'nimna'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'gitlove'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
@@ -39,8 +40,41 @@ def index():
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     return render_template('sign_in.html')
+    
+@app.route('/login_action', methods=['POST'])
+def login_action():
+    username = request.form['username']
+    password = request.form['password']
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * FROM users WHERE `username` = '%s' and `password` = '%s'" % (username, password))
+    data = cursor.fetchone()
+    if data is None:
+        return "Username or password is wrong."
+    else:
+        session['logged_in'] = True
+        return "Login sucessful!"
 
-# app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug = True)
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    return render_template('sign_up.html')
+    
+@app.route('/sign_up_action', methods=['POST'])
+def sign_up_action():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+    if password == confirm_password:
+        cursor = mysql.connect().cursor()
+        cursor.execute("INSERT INTO users (`username`, `email`, `password`) VALUES ('%s', '%s', '%s')" % (username, email, password))
+        mysql.connect().commit()
+        print "Profile created successfully."
+    else:
+        print "Passwords don't match."
+    return render_template('index.html')
+            
+
+app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug = True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
